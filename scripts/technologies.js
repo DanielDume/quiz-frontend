79403713fb6b41e9fc872c0d0d744d2968394913
ENDAVA\bchetan <Bogdan.Chetan@endava.com>
2017-11-24 10:27:12 +0200

    Technologies get done

--- scripts/technologies.js
+++ scripts/technologies.js
@@ -1,24 +1,57 @@
-
-
-
-
+var token = window.localStorage.getItem("token");
 $('#submit').on('click',function () {
 
     addTechnology();
 });
 
+$('#searchBtn').keyup(function () {
+
+    searchTechnology();
+});
+
+$(document).ready(function() {
+    getAllTechnologies();
+});
+
+function getAllTechnologies() {
+    $.ajax({
+        url: 'https://quiz-shm.herokuapp.com/api/technologies',
+        headers:{'x-access-token' : token},
+        success: function(data){
+            console.log(data);
+            populateTechnologyList(data);
+        },
+        error: function(){
+            console.log("BAD SHIT");
+        }
+    });
+}
+
+function populateTechnologyList(data){
+    var unorderedList = $('#technologyList');
+    var html = "";
+    $.each(data,function(index,value){
+        console.log(value.name);
+        html += '<article id=' + value._id + '><h3>'+ value.name + "<button class='btnTechnologies'>Delete</button><button class='btnTechnologies'>Update</button></h3></article>";
+    });
+    unorderedList.html(html);
+
+
+}
 function addTechnology() {
     var name = $("#technologies").val();
 
+
     $.ajax({
         url: "https://quiz-shm.herokuapp.com/api/technologies",
-        contentType: "application/x-www-form-urlencoded",
+        contentType:'application/x-www-form-urlencoded; charset=UTF-8',
         dataType: 'json',
         method: "POST",
+        headers:{'x-access-token' : token},
         data: {"name":name},
-        beforeSend: function(xhr){
-            xhr.setRequestHeader("X-Auth-Token", window.localStorage.getItem("token"));
-        },
+        // beforeSend: function(xhr){
+        //     xhr.setRequestHeader("X-Auth-Token", window.localStorage.getItem("token"));
+        // },
         success: function (data) {
             console.log(data.message);
         },
@@ -29,22 +62,15 @@ function addTechnology() {
 
 }
 
-$('#searchBtn').keyup(function () {
 
-    searchTechnology();
-});
 function searchTechnology() {
     var name = $("#searchBtn").val();
-
     $.ajax({
         url: "https://quiz-shm.herokuapp.com/api/technologies?name=" + name,
         contentType: "application/json",
         dataType: 'json',
-        authorization: window.localStorage.getItem("token"),
         method: "GET",
-        beforeSend: function(xhr){
-            xhr.setRequestHeader("X-Auth-Token", window.localStorage.getItem("token"));
-        },
+        headers:{'x-access-token' : token},
         success: function (data) {
             console.log(data.message);
         },
