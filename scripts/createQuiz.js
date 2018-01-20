68748b6b823aaeaf88c852048ba659b47f3aea4b
Daniel <dume.daniel@ymail.com>
2018-01-20 16:20:06 +0200

    Added create quiz frontend page

---
+++ scripts/createQuiz.js
@@ -0,0 +1,95 @@
+var server_url = "https://quiz-shm.herokuapp.com";
+
+function populateSelectTechnologies() {
+    var select = document.getElementById("technologySelect");
+    //var selectUpdate = document.getElementById("technologyUpdate");
+    $.ajax({
+        url: server_url + '/api/technologies',
+        headers: { 'x-access-token': window.localStorage.getItem("token") },
+        success: function (data) {
+            for (var i in data) {
+                var option1 = document.createElement("option");
+                option1.innerHTML = data[i]['name'];
+                option1.value = data[i]['_id'];
+                select.appendChild(option1);
+            }
+        },
+        error: function () {
+            console.log("error");
+        }
+    });
+}
+
+function populateSelectDifficulties() {
+    var select = document.getElementById("difficultySelect");
+    $.ajax({
+        url: server_url + '/api/difficulties',
+        headers: { 'x-access-token': window.localStorage.getItem("token") },
+        success: function (data) {
+            for (var i in data) {
+                var option1 = document.createElement("option");
+                option1.innerHTML = data[i]['name'];
+                option1.value = data[i]['_id'];
+                select.appendChild(option1);
+            }
+        },
+        error: function () {
+            console.log("error");
+        }
+    })
+}
+
+function populateSelectUser() {
+    var select = document.getElementById("userSelect");
+    $.ajax({
+        url: server_url + '/api/users',
+        headers: { 'x-access-token': window.localStorage.getItem("token") },
+        success: function (data) {
+            for (var i in data) {
+                console.log(data[i]['role'].toLowerCase());
+                if (data[i]['role'].toLowerCase() == "examinee") {
+                    var option1 = document.createElement("option");
+                    option1.innerHTML = data[i]['username'];
+                    option1.value = data[i]['_id'];
+                    select.appendChild(option1);
+                }
+
+            }
+        },
+        error: function () {
+            console.log("error");
+        }
+    })
+}
+
+$(document).ready(function () {
+    populateSelectDifficulties();
+    populateSelectTechnologies();
+    populateSelectUser();
+    $("#submit").on('click', function(){
+        let userId = $("#userSelect").val();
+        let time = $("#timeSelect").val();
+        let diffLevel = $("#difficultySelect").val();
+        let tech = $("#technologySelect").val();
+        $.ajax({
+            url: server_url + '/api/create-quiz/',
+            method: "POST",
+            headers: {'x-access-token': window.localStorage.getItem("token")},
+            contentType: "application/x-www-form-urlencoded",
+            data: {
+                difficultyLevelId: diffLevel,
+                technologyId: tech,
+                examineeId: userId,
+                timeToAnswer: time
+             },
+            success: function () {
+                
+            },
+            error: function (data) {
+                alert("Failed to create quiz, " + data.responseJSON.message);
+                console.log(data);
+            }
+        });
+    });
+
+})
